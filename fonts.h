cd28acadb93ce5477f828cb1254e6bf2397e82b1
haifenghuang <hai-feng.huang@hp.com>
2015-02-07 09:16:38 +0800

    Library's own fonts(header file)

---
+++ fonts.h
@@ -0,0 +1,10 @@
+#ifndef __ASCII_FONT__H
+#define __ASCII_FONT__H
+
+extern const char banner_ascii[];
+extern const char colossal_ascii[];
+extern const char univers_ascii[];
+extern const char bubble_ascii[];
+extern const char blocks_ascii[];
+
+#endif
